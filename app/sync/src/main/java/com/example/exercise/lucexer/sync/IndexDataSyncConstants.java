107456dd30fea788e1a42d5ea1603978245b2d7a
1200609 <hua.fan@ly.com>
2020-03-12 01:15:08 +0800

    update

---
+++ app/sync/src/main/java/com/example/exercise/lucexer/sync/IndexDataSyncConstants.java
@@ -0,0 +1,17 @@
+package com.example.exercise.lucexer.sync;
+
+/**
+ * IndexDataSyncConstants
+ *
+ * @author Deven
+ * @version : IndexDataSyncConstants, v 0.1 2020-03-11 13:35 Deven Exp$
+ */
+public class IndexDataSyncConstants {
+
+    public static final String COURSE_NAME_YUWEN = "语文";
+
+    public static final String COURSE_NAME_SHUXUE = "数学";
+
+    public static final String COURSE_NAME_HUAXUE = "化学";
+
+}
